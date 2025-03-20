b9cd7c5ad461ffc4a5d7c76147eb0e05ac678e32
Ian Chen <ianchen@IandeMacBook-Pro.local>
2025-03-21 01:41:33 +0800

    Update route setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom"
 import SlideViewer from "./components/SlideViewer"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/at-slides/:id" element={<SlideViewer />} />
-    </Routes>
+    <div>
+      <h1>Slide Demo</h1>
+      <ul>
+        <li>
+          <Link to="/at-slides/1">Slide 1</Link>
+        </li>
+        <li>
+          <Link to="/at-slides/2">Slide 2</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import App from "./App"
-import "./index.css"
+import SlideViewer from "./components/SlideViewer"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+      {" "}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/at-slides/:id" element={<SlideViewer />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 )
